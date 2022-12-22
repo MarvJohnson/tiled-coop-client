c8a4fb5a8c748bf816d4c9e442d8e68388c88c81
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-12-22 13:56:02 -0800

    Updated Serverless Function

--- api/user-auth.js
+++ api/user-auth.js
@@ -16,13 +16,15 @@ const pusher = new Pusher({
 });
 
 export default async (req, res) => {
-  const { socket_id: socketID, ...restBody } = req.body;
-  const { username: user, ...restParams } = req.params;
+  // const { socket_id: socketID, ...restBody } = req.body;
+  // const { username: user, ...restParams } = req.params;
   console.log("rest body");
-  console.log(JSON.stringify(restBody));
-  console.log(JSON.stringify(restParams));
+  console.log(JSON.stringify(req.body));
+  console.log(JSON.stringify(req.params));
+  console.log(JSON.stringify(req.query));
 
-  const authResponse = pusher.authenticateUser(socketID, { id: user });
+  // const authResponse = pusher.authenticateUser(socketID, { id: user });
 
-  res.status(200).end(authResponse);
+  // res.status(200).end(authResponse);
+  res.status(200).end();
 };
