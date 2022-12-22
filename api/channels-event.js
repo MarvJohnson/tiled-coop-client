173a711cd448b098ec98e0a835fd3f91dc9a6ace
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-12-22 15:46:17 -0800

    Updated Serverless Function

--- api/channels-event.js
+++ api/channels-event.js
@@ -14,11 +14,22 @@ const channels = new Channels({
   cluster,
 });
 
+const activeChannels = {};
+
 export default async (req, res) => {
   const { socketID, username: user, server, action, payload } = req.body;
 
   switch (action) {
     case "connection":
+      if (!activeChannels[server]) {
+        activeChannels[server] = {};
+      }
+
+      activeChannels[server][socketID] = {
+        user,
+        currentLayer: payload.initialLayer,
+      };
+
       await channels.trigger(
         server,
         "new_user_connected",
@@ -28,8 +39,14 @@ export default async (req, res) => {
         },
         { socket_id: socketID }
       );
+
+      channels.sendToUser(payload.userID, "new_connection_payload", {
+        channel: activeChannels[server],
+      });
       break;
     case "layer_changed":
+      activeChannels[server][socketID].currentLayer = payload.newLayer;
+
       await channels.trigger(
         server,
         "user_layer_changed",
