a1b5d710d95f408a4b1d4e4ea94dde4b9cb1450d
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-12-23 10:31:53 -0800

    Updated Serverless Function

--- api/pusher-webhooks.js
+++ api/pusher-webhooks.js
@@ -5,9 +5,13 @@ export default async (req, res) => {
 
   switch (events[0].name) {
     case "channel_vacated":
-      const userID = events
-        .find((event) => event.channel.includes("#"))
-        .channel.replace("#server-to-user-", "");
+      const userIDEvent = events.find((event) => event.channel.includes("#"));
+
+      if (!userIDEvent) {
+        break;
+      }
+
+      const userID = userIDEvent.channel.replace("#server-to-user-", "");
       await fetch(
         "https://tiled-coop-client-menthus123.vercel.app/api/channels-event/",
         {
