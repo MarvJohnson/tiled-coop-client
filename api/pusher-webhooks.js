bc1b7ff609dac4fbef86ba5ae7050ce86b683d4f
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-12-22 17:46:32 -0800

    Updated Serverless Function

--- api/pusher-webhooks.js
+++ api/pusher-webhooks.js
@@ -1,7 +1,7 @@
 export default async (req, res) => {
   console.log(JSON.stringify(req.body));
-  if (this.activeChannels) {
-    console.log(JSON.stringify(this.activeChannels));
+  if (activeChannels) {
+    console.log(JSON.stringify(activeChannels));
   } else {
     console.log("no global");
   }
