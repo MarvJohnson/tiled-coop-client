a2babe4f7e4d59a745682ca258efa88b9ffed259
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-12-25 19:04:17 -0800

    Updated Serverless Function

--- api/channel-auth.js
+++ api/channel-auth.js
@@ -2,28 +2,32 @@ export default async (req, res) => {
   console.log(JSON.stringify(req.body));
   const { socket_id: socketID, channel_name: channel, password } = req.body;
 
-  const response = await fetch(
-    "https://tiled-coop-client-menthus123.vercel.app/api/channels-event/",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        action: "channel_auth",
-        socketID,
-        channel,
-        payload: {
-          password,
+  try {
+    const response = await fetch(
+      "https://tiled-coop-client-menthus123.vercel.app/api/channels-event/",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          action: "channel_auth",
+          socketID,
+          channel,
+          payload: {
+            password,
+          },
+        }),
+        headers: {
+          "Content-Type": "application/json",
         },
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+      }
+    );
 
-  const json = await response.json();
+    const json = await response.json();
 
-  console.log("Response from channel-event");
-  console.log(json);
+    console.log("Response from channel-event");
+    console.log(json);
 
-  return res.send(json);
+    return res.send(json);
+  } catch (err) {
+    return res.status(403).end();
+  }
 };
