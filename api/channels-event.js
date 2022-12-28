fa59d95536bec605d045955335a582215430aed4
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-12-27 16:05:06 -0800

    Updated Serverless Function

--- api/channels-event.js
+++ api/channels-event.js
@@ -180,6 +180,7 @@ export default async (req, res) => {
         console.log("error", err);
         console.log("fields", fields);
         console.log("files", files);
+        console.log(JSON.stringify(JSON.parse(fields.payload)));
       });
       break;
   }
