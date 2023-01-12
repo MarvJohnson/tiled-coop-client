0248aecc2aa8eea34289e4d9ab1f97d5888242c8
Marvel Johnson <MarvelJohnson@Hotmail.com>
2023-01-12 13:43:24 -0800

    Updated Serverless Function

--- api/sync.ts
+++ api/sync.ts
@@ -12,11 +12,15 @@ export default async (req: Request) => {
   const formData = await req.formData();
   formData.append("api_dev_key", PASTEBIN_API_KEY);
 
-  return await fetch("https://pastebin.com/api/api_post.php", {
+  const response = await fetch("https://pastebin.com/api/api_post.php", {
     method: "POST",
     body: formData,
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
+
+  console.log(response);
+
+  return new Response(response.body);
 };
